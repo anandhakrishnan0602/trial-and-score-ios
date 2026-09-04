37064e3be4c9c38a4de9669a2c42914d8702426c
Anandhakrishnan <>
2026-09-05 02:36:39 +0530

    add ClinicalTrialsRepository

---
+++ TrialAndScore/TrialAndScore/Repository/ClinicalTrialsRepository.swift
@@ -0,0 +1,20 @@
+//
+//  ClinicalTrialsRepository.swift
+//  TrialAndScore
+//
+//  Created by Anandhakrishnan on 05/09/26.
+//
+
+final class ClinicalTrialsRepository {
+    private let service: ClinicalTrialsService
+
+    init(service: ClinicalTrialsService = ClinicalTrialsService()) {
+        self.service = service
+    }
+
+    func fetchTrials(condition: String) async throws -> [Trial] {
+        let trialResponse = try await service.fetchTrials(condition: condition)
+        let trials = trialResponse.studies.compactMap { Trial(study: $0) }
+        return trials
+    }
+}
