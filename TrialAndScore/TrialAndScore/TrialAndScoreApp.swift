ecf79785ae4f937cdfe7cae2ea90c3a5525ba9a9
Anandhakrishnan <>
2026-09-04 21:43:31 +0530

    Initial Xcode project setup, iOS 15 target

---
+++ TrialAndScore/TrialAndScore/TrialAndScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrialAndScoreApp.swift
+//  TrialAndScore
+//
+//  Created by Anandhakrishnan on 04/09/26.
+//
+
+import SwiftUI
+
+@main
+struct TrialAndScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
